91d6528b7e7f298b19476cb2fd3619a372ab7248
Carolerilog <dingyongyu2002@foxmail.com>
2023-08-19 17:00:11 +0800

    1007 pass

---
+++ 1007_Maximum_Subsequence_Sum.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> num(n);
+    for(auto& n : num) {
+        cin >> n;
+    }
+
+    int l = 0, r = 0, sum = 0;
+    int max_l = 0, max_r = 0, max_sum = 0x80000000;
+    while(r < n) {
+        sum += num[r];
+
+        if(sum > max_sum) {
+            max_l = l;
+            max_r = r;
+            max_sum = sum;
+        }
+
+        if(sum < 0) {
+            l = r + 1;
+            sum = 0;
+        }
+
+        r++;
+    }
+
+    if(max_sum < 0) {
+        max_sum = 0;
+        max_l = 0;
+        max_r = n - 1;
+    }
+
+    cout << max_sum << " " << num[max_l] << " " << num[max_r] << endl;
+}
